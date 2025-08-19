be168669805531e7906f0f9312c8f7868539436b
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-08-19 17:06:55 -0600

    add head instance variable for LinkedList, add method append which at the moment just creates the head element, fix wrong instance variables in Node, add instance of linked_list and call append method

--- main.rb
+++ main.rb
@@ -1,9 +1,26 @@
 class LinkedList
+  attr_accessor :head
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    if head == nil
+      node = Node.new(value, nil)
+      @head = node
+      puts "add value #{value} as the head node"
+    else
+      # code for adding new nodes beside head
+    end  
+  end
 end
 
 class Node
   def initialize(value, next_node)
-    value = value
-    next_node = next_node
+    @value = value
+    @next_node = next_node
   end
 end
+
+linked_list = LinkedList.new()
+linked_list.append(1)
